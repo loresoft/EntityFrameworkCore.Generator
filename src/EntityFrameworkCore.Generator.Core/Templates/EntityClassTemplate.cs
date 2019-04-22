d4ed81647cc7f772edf30d4f28cc07d103318cd7
Paul Welter <pwelter@loresoft.com>
2019-04-22 13:56:23 -0500

    sort relationships to prevent source control churn

--- src/EntityFrameworkCore.Generator.Core/Templates/EntityClassTemplate.cs
+++ src/EntityFrameworkCore.Generator.Core/Templates/EntityClassTemplate.cs
@@ -65,6 +65,7 @@ namespace EntityFrameworkCore.Generator.Templates
         {
             var relationships = _entity.Relationships
                 .Where(r => r.Cardinality == Cardinality.Many)
+                .OrderBy(r => r.PropertyName)
                 .ToList();
 
             var entityClass = _entity.EntityClass.ToSafeName();
@@ -128,7 +129,7 @@ namespace EntityFrameworkCore.Generator.Templates
         private void GenerateRelationshipProperties()
         {
             CodeBuilder.AppendLine("#region Generated Relationships");
-            foreach (var relationship in _entity.Relationships)
+            foreach (var relationship in _entity.Relationships.OrderBy(r => r.PropertyName))
             {
                 var propertyName = relationship.PropertyName.ToSafeName();
                 var primaryName = relationship.PrimaryEntity.EntityClass.ToSafeName();
