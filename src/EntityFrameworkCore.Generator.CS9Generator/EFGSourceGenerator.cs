0dfa4e06af5f10a0a181bc0fbcff55d7fc5357d9
Eugene Bekker <ebekker@users.noreply.github.com>
2020-12-14 19:16:23 -0500

    First stab at CS9 source code generator:
    * basically mimics the same behavior as the CLI, only during src code gen invocation
    * this first version still just writes out files to disk, same as the CLI

---
+++ src/EntityFrameworkCore.Generator.CS9Generator/EFGSourceGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EntityFrameworkCore.Generator.CS9Generator
+{
+    [Generator]
+    public class EFGSourceGenerator : ISourceGenerator
+    {
+        private IServiceProvider _services;
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            _services = new ServiceCollection()
+                .AddLogging()
+                .AddTransient<IGeneratorOptionsSerializer, GeneratorOptionsSerializer>()
+                .AddTransient<ICodeGenerator, CodeGenerator>()
+                .AddTransient<IModelCacheBuilder, ModelCacheBuilder>()
+                .BuildServiceProvider();
+        }
+        
+        public void Execute(GeneratorExecutionContext context)
+        {
+            IGeneratorOptionsSerializer gos = _services.GetRequiredService<IGeneratorOptionsSerializer>();
+            ICodeGenerator cg = _services.GetRequiredService<ICodeGenerator>();
+
+            foreach (var addTxt in context.AdditionalFiles)
+            {
+                if (addTxt.Path.EndsWith(".efg.yml") || addTxt.Path.EndsWith(".efg.yaml"))
+                {
+                    var fullPath = addTxt.Path;
+                    var directory = Path.GetDirectoryName(fullPath);
+                    var fileName = Path.GetFileName(fullPath);
+                    var fileNameWOext = Path.GetFileNameWithoutExtension(fullPath);
+                    if (fileNameWOext.EndsWith(".efg", StringComparison.OrdinalIgnoreCase))
+                        fileNameWOext = Path.GetFileNameWithoutExtension(fileNameWOext);
+
+                    // throw new Exception("GENERATED: " + fileNameWOext);
+
+                    context.AddSource($"EFG_{fileNameWOext}", $@"
+namespace EFG.Generated
+{{
+    class {fileNameWOext}
+    {{
+        public const string Name = ""{fileNameWOext}"";
+    }}
+}}");
+
+                    var options = gos.Load(directory, fileName);
+                    cg.Generate(options, fromCache: true, updateFromSource: false);
+                }
+            }
+        }
+    }
+}
