985005c8608b6c1c33355d760ad30812d97012aa
Paul Welter <pwelter@loresoft.com>
2023-07-28 21:33:41 -0500

    fix package error

--- sample/Tracker/Tracker.Scaffold/TrackerContext.cs
+++ sample/Tracker/Tracker.Scaffold/TrackerContext.cs
@@ -36,7 +36,6 @@ public partial class TrackerContext : DbContext
     public virtual DbSet<UserLogin> UserLogins { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=(local)\\sql2022;Initial Catalog=Tracker;Integrated Security=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
